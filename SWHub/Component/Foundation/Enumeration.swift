3f487451e8269e21d7932f0100e3d30f9d387581
yangjianxiang <yangjianxiang@cd.tuan800.com>
2020-05-04 11:55:48 +0800

    重组名

--- SWHub/Component/Foundation/Enumeration.swift
+++ SWHub/Component/Foundation/Enumeration.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2020 杨建祥. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum TrendingSince {
+    case daily, weekly, monthly
+
+//    var title: String {
+//        switch self {
+//        case .daily: return R.string.localizable.searchDailySegmentTitle.key.localized()
+//        case .weekly: return R.string.localizable.searchWeeklySegmentTitle.key.localized()
+//        case .montly: return R.string.localizable.searchMonthlySegmentTitle.key.localized()
+//        }
+//    }
+
+    var value: String {
+        switch self {
+        case .daily: return "daily"
+        case .weekly: return "weekly"
+        case .monthly: return "monthly"
+        }
+    }
+    
+}
